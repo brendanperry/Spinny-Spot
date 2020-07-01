b4f5852165ffbbf64f5e359de53ab3127527fe75
Brendan Perry <brendan@brendanperry.me>
2020-07-01 17:51:39 -0400

    current version live on app store

--- Spinny Spot/Assets/Scripts/RatePopUp.cs
+++ Spinny Spot/Assets/Scripts/RatePopUp.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RatePopUp : MonoBehaviour {
 
-    string appleId = "itms-apps://itunes.apple.com/spinny-spot/id1399550437?ls=1&mt=8";
+    string appleId = "https://apps.apple.com/us/app/id1399550437";
 	
 	public void ShowRatePopUp() {
         Application.OpenURL(appleId);
